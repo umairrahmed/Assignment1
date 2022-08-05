71ffaaf19ab067f3659d8db2823327b3e9a80fe1
Umair Ahmed <umairahmedpaki7@gmail.com>
2022-08-05 18:51:31 +0500

    Icon collapse and padding issue resolved

--- scroll.js
+++ scroll.js
@@ -3,15 +3,6 @@ window.addEventListener('scroll',function(){
     navbar.classList.toggle('navscroll',window.scrollY>100);
 })
 
-window.addEventListener('scroll',function(){
-    let navbar=this.document.querySelectorAll('ion-icon');
-    navbar[0].classList.toggle('blackicon',window.scrollY>460);
-    navbar[1].classList.toggle('blackicon',window.scrollY>475);
-    navbar[2].classList.toggle('blackicon',window.scrollY>500);
-    navbar[3].classList.toggle('blackicon',window.scrollY>535);
-
-
-})
 
 
 
